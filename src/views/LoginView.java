1ccf4490e7a39b3dae20a81592e4f889a47c075a
JoelTorres981 <joel.torres@epn.edu.ec>
2025-07-28 22:30:19 -0500

    Vista login

--- src/views/LoginView.java
+++ src/views/LoginView.java
@@ -1,4 +1,20 @@
 package views;
 
-public class LoginView {
+import javax.swing.*;
+
+public class LoginView extends JFrame{
+    private JPanel loginPanel;
+    private JTextField usuarioText;
+    private JPasswordField password;
+    private JButton iniciarSesionButton;
+    private JButton crearCuentaButton;
+    private JLabel recuperarContraseñaLabel;
+
+    public LoginView(){
+        setTitle("Login");
+        setContentPane(loginPanel);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(600,400);
+        setLocationRelativeTo(null);
+    }
 }
